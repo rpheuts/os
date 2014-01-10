098806a91a73d447c4fdf1caca85d294092c7bf0
Robert Heuts <rpheuts@gmail.com>
2014-01-10 13:54:43 -0800

    Cleaned up script

--- qemu/imagegen.sh
+++ qemu/imagegen.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Make a RAM disk of 1.44 MB as ExFAT
+# Make a disk-image of 1.44 MB
 NUMSECTORS=51200
 DEVNAME="disk.img"
 
 # Create image
 dd if=/dev/zero bs=1024 count=${NUMSECTORS} > ${DEVNAME}
-
-# Partition image
-fdisk -yia dos ${DEVNAME}
 
 # Copy stage0 bootloader
 fdisk -yu -f ../src/stage0/stage0.bin ${DEVNAME}
